a57deedcf0af14935dbdfcc85ae41a29ad389553
jzara3115 <jzara3115@gmail.com>
2020-09-01 23:19:20 -0700

    camera movements and third person
    Added cinematic camera and third person view.  imported level.

--- Crowd Gathering Game/Assets/Scripts/movement.cs
+++ Crowd Gathering Game/Assets/Scripts/movement.cs
@@ -6,9 +6,14 @@ public class movement : MonoBehaviour
 {
 
     private Rigidbody rb;
+    // public CharacterController charController;
     public float speed;
     private float moveInput;
     private float moveInput2;
+    public Transform cam;
+
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
 
 
 
@@ -25,11 +30,19 @@ public class movement : MonoBehaviour
     void Update()
     {
             moveInput = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector3(moveInput * speed, rb.velocity.y, rb.velocity.z);
-            
             moveInput2 = Input.GetAxis("Vertical");
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, moveInput2 * speed);
+            //rb.velocity = new Vector3(moveInput * speed, rb.velocity.y,  moveInput2 * speed);
+            Vector3 direction = new Vector3(moveInput, 0f, moveInput2).normalized;
         
+
+            if (direction.magnitude >= 0.1f) {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation= Quaternion.Euler(0f,angle,0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                rb.velocity = (moveDir * speed);
+            }
     }
 
     void OnTriggerEnter(Collider cldr)
